5e5627de9ac00ea9c6ac88af2ddc5568312cccc0
Alexander Zinchuk <alx.zinchuk@gmail.com>
2018-07-28 21:17:41 +0300

    To Do List Page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  tasks = [{
+    name: 'Hello'
+  }, {
+    name: 'Toptal'
+  }, {
+    name: 'Blog'
+  }];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public alertCtrl: AlertController) {}
 
+  addTask() {
+    this.alertCtrl
+      .create({
+        title: 'Add Task',
+        inputs: [
+          {
+            name: 'name',
+            placeholder: 'Task'
+          }
+        ],
+        buttons: [
+          {
+            text: 'Cancel',
+            role: 'cancel'
+          },
+          {
+            text: 'Add',
+            handler: ({ name }) => {
+              this.tasks.push({ name });
+            }
+          }
+        ]
+      })
+      .present();
   }
-
 }
